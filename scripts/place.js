1b2ef590c423a137cd06beb16df8c8f89e0177fc
benjaminewoh <benjaminfridayewoh18@gmail.com>
2026-09-15 10:17:50 +0100

    Complete WDD 131 Week 03 Country Page

---
+++ scripts/place.js
@@ -0,0 +1,34 @@
+// ==============================
+// W03 COUNTRY PAGE JAVASCRIPT
+// ==============================
+
+// Static weather values
+const temperature = 28;
+const windSpeed = 10;
+
+
+// Calculate wind chill in Celsius
+function calculateWindChill(temp, speed) {
+    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
+}
+
+
+// Display wind chill only when conditions are met
+const windChillElement = document.querySelector("#wind-chill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChillElement.textContent =
+        `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+} else {
+    windChillElement.textContent = "N/A";
+}
+
+
+// Current year
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+
+// Last modified date
+document.querySelector("#lastModified").textContent =
+    document.lastModified;
